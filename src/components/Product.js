3b73d87aa8f457e7a52dcab4fce3d2e76b9e8903
Aidil <aidilrozi83@gmail.com>
2021-08-13 18:22:48 -0500

    Finished styling the ProductFeed component.

--- src/components/Product.js
+++ src/components/Product.js
@@ -32,7 +32,7 @@ function Product({ product }) {
         <Currency quantity={price} currency="USD" />
       </div>
       {hasPrime && (
-        <div className="flex items-center space-x-2 mt-5">
+        <div className="flex items-center space-x-2 -mt-5">
           <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
